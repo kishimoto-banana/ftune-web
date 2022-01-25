57916df407c743748386f6830751c50962016344
kishimoto <drehbleistift@gmail.com>
2022-01-25 17:07:29 +0900

    頻度とかの追加

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,16 @@
-import { FREQ_EN_JA } from "../lib/constants";
+import { FREQ_EN_JA, TYPE_EN_JA } from "../lib/constants";
 
-const Card = ({ title, media, body, frequency, date, imageUrl }) => {
+const Card = ({ title, media, body, frequency, type, date, imageUrl }) => {
   return (
     <div class="max-w-xs bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <img
-          class="rounded-t-lg object-cover h-40 w-80"
+          class="rounded-t-lg object-cover h-32 w-96"
           src={imageUrl}
           alt=""
         />
       </a>
-      <div class="p-5">
+      <div class="p-2">
         <a href="#">
           <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
             {title}
@@ -19,15 +19,20 @@ const Card = ({ title, media, body, frequency, date, imageUrl }) => {
         <p class="p-1 text-sm text-left font-normal text-gray-400 dark:text-gray-400">
           {media}
         </p>
-        <p class="truncate mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p class="truncate mb-1 font-normal text-gray-700 dark:text-gray-400">
           {body}
-        </p>
-        <p class="p-1 text-sm font-normal text-gray-700 dark:text-gray-400">
-          {FREQ_EN_JA[frequency]}
         </p>
         <p class="p-1 text-sm font-normal text-gray-700 dark:text-gray-400">
           {date}
         </p>
+        <div class="px-6 pt-4">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #{FREQ_EN_JA[frequency]}
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #{TYPE_EN_JA[type]}
+          </span>
+        </div>
       </div>
     </div>
   );
