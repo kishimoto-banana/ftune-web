143ce09873ebba5645300dd7e0ffacddcf102ffb
kishimoto <drehbleistift@gmail.com>
2022-01-28 22:06:16 +0900

    とりあえず雛形

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Navigation from "../components/Navigation";
+
+// export async function getServerSideProps() {
+//   const uranais = await getUranais("20220126", "scorpio");
+//   return { props: { uranais } };
+// }
+
+const About = () => {
+  return (
+    <div className="flex flex-col min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navigation />
+
+      <main className="flex flex-col items-center w-full flex-1 px-10 text-center">
+        <p>Ftuneとはね…!?!?!?????!??!!!?</p>
+      </main>
+    </div>
+  );
+};
+
+export default About;
