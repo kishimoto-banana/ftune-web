ab6b6a3d034608b7a8cb413d9cda56dbdca4aaff
kishimoto <drehbleistift@gmail.com>
2022-01-28 22:06:02 +0900

    class => className

--- components/Card.js
+++ components/Card.js
@@ -2,34 +2,34 @@ import { FREQ_EN_JA, TYPE_EN_JA } from "../lib/constants";
 
 const Card = ({ title, media, body, frequency, type, date, imageUrl }) => {
   return (
-    <div class="max-w-xs bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-xs bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <img
-          class="rounded-t-lg object-cover h-32 w-96"
+          className="rounded-t-lg object-cover h-32 w-96"
           src={imageUrl}
           alt=""
         />
       </a>
-      <div class="p-2">
+      <div className="p-2">
         <a href="#">
-          <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
             {title}
           </h5>
         </a>
-        <p class="p-1 text-sm text-left font-normal text-gray-400 dark:text-gray-400">
+        <p className="p-1 text-sm text-left font-normal text-gray-400 dark:text-gray-400">
           {media}
         </p>
-        <p class="truncate mb-1 font-normal text-gray-700 dark:text-gray-400">
+        <p className="truncate mb-1 font-normal text-gray-700 dark:text-gray-400">
           {body}
         </p>
-        <p class="p-1 text-sm font-normal text-gray-700 dark:text-gray-400">
+        <p className="p-1 text-sm font-normal text-gray-700 dark:text-gray-400">
           {date}
         </p>
-        <div class="px-6 pt-4">
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        <div className="px-6 pt-4">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             #{FREQ_EN_JA[frequency]}
           </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             #{TYPE_EN_JA[type]}
           </span>
         </div>
--- components/Form.js
+++ components/Form.js
@@ -7,27 +7,27 @@ const Form = ({
   inValidBirthDay,
 }) => {
   return (
-    <form class="grid grid-cols-1 gap-2 pb-4 pt-4">
-      <label class="block">
-        <span class="text-gray-700">生年月日</span>
+    <form className="grid grid-cols-1 gap-2 pb-4 pt-4">
+      <label className="block">
+        <span className="text-gray-700">生年月日</span>
         <input
           type="date"
           value={birthday}
           onChange={handleBirthdayChange}
-          class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         />
         {inValidBirthDay && (
-          <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+          <p className="mt-2 text-sm text-red-600 dark:text-red-500">
             生年月日が正しくなさそうです
           </p>
         )}
       </label>
-      <label class="block">
-        <span class="text-gray-700">性別</span>
+      <label className="block">
+        <span className="text-gray-700">性別</span>
         <select
           value={gender}
           onChange={handleGenderChange}
-          class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          className="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         >
           <option value="女性">女性</option>
           <option value="男性">男性</option>
@@ -37,7 +37,7 @@ const Form = ({
       <button
         type="button"
         onClick={handleFormConfirm}
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         送信
       </button>
