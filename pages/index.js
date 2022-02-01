87d60f25b0438f802ba0de98ca49a003b465c4b8
kishimoto <drehbleistift@gmail.com>
2022-02-01 13:35:54 +0900

    生年月日未入力ではランキングが出るように

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
+import format from "date-fns/format";
 import Head from "next/head";
 import FormContainer from "../components/FormContainer";
-import UranaiListContainer from "../components/UranaiListContainer";
+import Loading from "../components/Loading";
 import Navigation from "../components/Navigation";
+import RankingList from "../components/Rankinkg";
+import UranaiListContainer from "../components/UranaiListContainer";
+import { useUser } from "../context/userContext";
+import { getAnalyzedUranai } from "../fetchData/nodeApp";
 
-// export async function getServerSideProps() {
-//   const uranais = await getUranais("20220126", "scorpio");
-//   return { props: { uranais } };
-// }
+export async function getServerSideProps() {
+  const today = format(new Date(), "yyyyMMdd");
+  const analyzedUranais = await getAnalyzedUranai(today);
+  const ranking = analyzedUranais.sort((a, b) => b.score - a.score);
+  return { props: { ranking } };
+}
 
-export default function Home() {
+export default function Home({ ranking }) {
+  const { user, loadingUser } = useUser();
+
   return (
     <div className="flex flex-col min-h-screen py-2">
       <Head>
@@ -19,7 +28,13 @@ export default function Home() {
 
       <main className="flex flex-col items-center w-full flex-1 px-10 text-center">
         <FormContainer />
-        <UranaiListContainer />
+        {loadingUser ? (
+          <Loading />
+        ) : user.birthday && user.gender ? (
+          <UranaiListContainer />
+        ) : (
+          <RankingList ranking={ranking} />
+        )}
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
