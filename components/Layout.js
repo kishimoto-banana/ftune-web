fe593b446cc5e858e09d2a40d297fc407f9e2e55
kishimoto <drehbleistift@gmail.com>
2022-02-05 20:01:55 +0900

    meta

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,22 @@ const Layout = ({ children }) => {
       <Head>
         <title>Ftune</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:site_name" content="Ftune" />
+        <meta property="og:title" content="Ftune" />
+        <meta
+          property="og:description"
+          content="複数の占いをまとめて確認できるサービスです。"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/logo512.png" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:image" content="logo512.png" />
+
+        <meta
+          name="description"
+          content="複数の占いをまとめて確認できるサービスです。"
+        />
       </Head>
       <Navigation />
 
