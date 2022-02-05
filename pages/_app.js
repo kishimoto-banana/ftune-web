ea57a0ed433488cc0d87de2ca1ca4a5b1846a4e1
kishimoto <drehbleistift@gmail.com>
2022-02-05 21:49:38 +0900

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@ import "../firebase/clientApp";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import UserProvider from "../context/userContext";
+import { GoogleAnalytics, usePageView } from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  usePageView();
+
   return (
     <UserProvider>
       <Layout>
+        <GoogleAnalytics />
         <Component {...pageProps} />
       </Layout>
     </UserProvider>
