afe4cbb3d316f751bda57396646f4887db683530
kishimoto <drehbleistift@gmail.com>
2022-02-06 12:23:20 +0900

    adsense

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,12 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { Adsense } from "../lib/adsense";
 import { GoogleAnalytics } from "../lib/gtag";
 
 export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        <Adsense />
         <GoogleAnalytics />
       </Head>
       <body>
