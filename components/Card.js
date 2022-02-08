2e48efd7f5575e2beb1738e0ccdd53fc1cac1f9e
kishimoto <drehbleistift@gmail.com>
2022-02-08 11:47:16 +0900

    占いのカードから直接外部サイトに

--- components/Card.js
+++ components/Card.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { FREQ_EN_JA, TYPE_EN_JA } from "../lib/constants";
 
 const Card = ({
@@ -15,55 +14,39 @@ const Card = ({
   imageUrl,
 }) => {
   return (
-    <Link
-      href={{
-        pathname: `/uranai/${fetchDate}-${sign}-${id}`,
-        query: {
-          title: title,
-          media: media,
-          url: url,
-          body: body,
-          date: date,
-          imageUrl: imageUrl,
-          sign: sign,
-        },
-      }}
-      as={`/uranai/${fetchDate}-${sign}-${id}`}
-    >
-      <a>
-        <div className="max-w-uranaiCard bg-white rounded-lg border border-gray-200 shadow-md">
-          <img
-            className="rounded-t-lg object-cover h-24 md:h-32 w-full"
-            src={imageUrl}
-            alt=""
-          />
-          <div className="p-2 md:p-4">
-            <h5 className="text-sm md:text-lg font-bold truncate text-gray-900">
-              {title}
-            </h5>
-            <p className="text-xs md:text-sm text-left font-normal text-gray-400">
-              {media}
-            </p>
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <div className="max-w-uranaiCard bg-white rounded-lg border border-gray-200 shadow-md">
+        <img
+          className="rounded-t-lg object-cover h-24 md:h-32 w-full"
+          src={imageUrl}
+          alt=""
+        />
+        <div className="p-2 md:p-4">
+          <h5 className="text-sm md:text-lg font-bold truncate text-gray-900">
+            {title}
+          </h5>
+          <p className="text-xs md:text-sm text-left font-normal text-gray-400">
+            {media}
+          </p>
 
-            <p className="pt-1 text-sm md:text-md truncate mb-1 font-normal text-gray-700">
-              {body}
-            </p>
+          <p className="pt-1 text-sm md:text-md truncate mb-1 font-normal text-gray-700">
+            {body}
+          </p>
 
-            <p className="pt-2 text-xs md:text-sm text-left font-normal text-gray-700">
-              {date}
-            </p>
-            <div className="pt-1">
-              <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs md:text-sm font-semibold text-gray-700 mr-1">
-                #{FREQ_EN_JA[frequency]}
-              </span>
-              <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs md:text-sm font-semibold text-gray-700">
-                #{TYPE_EN_JA[type]}
-              </span>
-            </div>
+          <p className="pt-2 text-xs md:text-sm text-left font-normal text-gray-700">
+            {date}
+          </p>
+          <div className="pt-1">
+            <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs md:text-sm font-semibold text-gray-700 mr-1">
+              #{FREQ_EN_JA[frequency]}
+            </span>
+            <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs md:text-sm font-semibold text-gray-700">
+              #{TYPE_EN_JA[type]}
+            </span>
           </div>
         </div>
-      </a>
-    </Link>
+      </div>
+    </a>
   );
 };
 
