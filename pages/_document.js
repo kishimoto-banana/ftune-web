256add302b57a2c5528d553ab1ff1196fe83a7c0
masashi kishimoto <drehbleistift@gmail.com>
2022-02-25 14:42:21 +0900

    nend入れてみる

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,21 @@
 import { Head, Html, Main, NextScript } from "next/document";
-import { Adsense } from "../lib/adsense";
 import { GoogleAnalytics } from "../lib/gtag";
+import Nend from "../lib/nend";
 
 export default function Document() {
   return (
     <Html lang="ja">
       <Head>
-        <Adsense />
         <GoogleAnalytics />
+        <Nend
+          params={{
+            media: 70036,
+            site: 354848,
+            spot: 1053096,
+            type: 2,
+            oriented: 1,
+          }}
+        />
       </Head>
       <body>
         <Main />
